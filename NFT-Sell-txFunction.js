c127bc6d4e4bbbcf41ee31bf6f6a83885a9202d9
Damon Cook <86455508+damonlrcook@users.noreply.github.com>
2021-11-23 12:20:06 +1000

    Update NFT-Sell-txFunction.js
    Added in the ability to cancel the sell order of an NFT when the input price is 0.

--- NFT-Sell-txFunction.js
+++ NFT-Sell-txFunction.js
@@ -14,7 +14,6 @@ module.exports = async (body) => {
   } else {
     var buyingAsset = new Asset(buyingCode, buyingIssuer);
   }
-  
 
   // Variable Horizon URL
   var sellingURL = HORIZON_URL + "/accounts/" + walletAddr;
@@ -23,16 +22,115 @@ module.exports = async (body) => {
   newQuantity = parseFloat(quantity).toFixed(0);
 
   // Make sure we are using integer sale value to protect nonfungible integrity
-  var remainder = quantity % 1
+  var remainder = quantity % 1;
   
   if (remainder !== 0 ) {
     throw {message: 'Amount must be an integer value i.e. 1 or 3 etc.'};
   } else if (quantity < 1) {
     throw {message: 'Please enter a number that is greater than one'};
   }
+  
+  
     
+  if (price == 0) {
+    //Find the offer number from the account.
+    var offerDetails = await findOffer(walletAddr, nftCode, nftIssuer);
+    
+    return cancelSell(walletAddr, nftIssuer, newPrice, sellingAsset, buyingAsset, newQuantity, sellingURL, offerDetails);
+  } else {
+    return normalSell(walletAddr, nftIssuer, newPrice, sellingAsset, buyingAsset, newQuantity, sellingURL);
+  }
+ 
+}
+
+async function findOffer(walletAddr, nftCode, nftIssuer){
+  var offersURL = HORIZON_URL + "/accounts/" + walletAddr + "/offers";
+  var offerID = "Nothing"
+  var offerAmount = "Nothing"
+  return await fetch(offersURL)
+  .then((res) => {
+      if (res.ok)
+          return res.json()
+      throw res
+      })
+  .then((issuer) => {
+      // Pulls the data in from the issuing account
+      data = Object.entries(issuer["_embedded"]["records"])
+      
+      for (let i = 0; i < data.length; i++) {
+          if (data[i][1]["selling"]["asset_code"] == nftCode && data[i][1]["selling"]["asset_issuer"] == nftIssuer){
+              offerID = data[i][1]["id"]
+              offerAmount = data[i][1]["price"]
+              
+          }
+      }
+
+      if (offerID == "Nothing") {
+          throw{message:"There appears to be no offer for this asset"}
+      }
+      
+      var retur = [offerID, offerAmount]
+      return retur
+  }); 
+}
+
+async function cancelSell(walletAddr, nftIssuer, newPrice, sellingAsset, buyingAsset, newQuantity, sellingURL, offerDetails) {
   // Build the transaction of the NFT
-  return fetch(sellingURL)
+  return await fetch(sellingURL)
+  .then((res) => {
+    if (res.ok)
+      return res.json()
+    throw res
+  })
+  
+  .then((account) =>
+    new TransactionBuilder(
+      new Account(account.id, account.sequence), 
+      { 
+        fee: BASE_FEE, 
+        networkPassphrase: Networks[STELLAR_NETWORK],
+      }
+    )
+   
+    // Authorise the account to have full authority with the asset
+    .addOperation(Operation.setTrustLineFlags({
+      trustor: walletAddr,
+      asset: sellingAsset,
+      flags: {
+        authorized:true,
+        authorizedToMaintainLiabilities: false
+      },
+      source: nftIssuer
+    }))
+
+    // Add the selling operation of the transaction
+    .addOperation(Operation.manageSellOffer({
+      selling: sellingAsset,
+      buying: buyingAsset,
+      amount: "0",
+      price: offerDetails[1],
+      offerId: offerDetails[0]
+    }))
+    
+    // Remove full authority and only authorise to maintain liabilities
+    .addOperation(Operation.setTrustLineFlags({
+      trustor: walletAddr,
+      asset: sellingAsset,
+      flags: {
+        authorized: false,
+        authorizedToMaintainLiabilities: true
+      },
+      source: nftIssuer
+    }))
+    .setTimeout(0)
+    .build()
+    .toXDR()
+  )
+}
+
+async function normalSell(walletAddr, nftIssuer, newPrice, sellingAsset, buyingAsset, newQuantity, sellingURL) {
+  // Build the transaction of the NFT
+  return await fetch(sellingURL)
   .then((res) => {
     if (res.ok)
       return res.json()
